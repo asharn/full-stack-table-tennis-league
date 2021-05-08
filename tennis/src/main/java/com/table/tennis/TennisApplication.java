d81c2c3baf1970774c99a06fb567d3a94b1d59fc
xeuser <xeuser@XEBIAINDIA.LOCAL>
2021-05-08 15:18:08 +0530

    First controller added for testing purpose

---
+++ tennis/src/main/java/com/table/tennis/TennisApplication.java
@@ -0,0 +1,13 @@
+package com.table.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisApplication.class, args);
+	}
+
+}
---
+++ tennis/src/test/java/com/table/tennis/TennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.table.tennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
