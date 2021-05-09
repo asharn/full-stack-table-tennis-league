aac0ab2546df9b3b8a0f25cfd0cc737d06bb45c3
ASHISH KARN <ASHISH.KARN@XEBIAINDIA.LOCAL>
2021-05-10 00:19:24 +0530

    Rest api tested on postman and get all applicants uri call.

--- tennis/src/main/java/com/table/tennis/api/ParticipantController.java
+++ tennis/src/main/java/com/table/tennis/api/ParticipantController.java
@@ -1,19 +1,22 @@
 package com.table.tennis.api;
 
+import com.table.tennis.contants.RestURIConstants;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.table.tennis.contants.RestURIConstants.PARTICIPANTS;
 
 @RestController
 public class ParticipantController {
 
-    @GetMapping("/participant")
+    @GetMapping(RestURIConstants.PARTICIPANTS)
     public String getParticipants(){
         return "Hello World participant";
     }
 
     @GetMapping("/")
     public String defaultMethod(){
-        return "Hello World";
+        return "Welcome to the game of table tennis!!!";
     }
 
 }
---
+++ tennis/src/main/java/com/table/tennis/contants/RestURIConstants.java
@@ -0,0 +1,6 @@
+package com.table.tennis.contants;
+
+public class RestURIConstants {
+    public static final String API_VERSION = "/v1";
+    public static final String PARTICIPANTS = API_VERSION + "/participants";
+}
